1efe6b7ab4a3a433ee99e213bfd8e8231286be86
TQA <trinhquocanh94@gmail.com>
2019-01-25 11:14:26 +0100

    Solve sum-of-multiples exercise.

--- sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,15 +1,9 @@
 object SumOfMultiples {
   def sum(factors: Set[Int], limit: Int): Int = {
-    val numbers = List.range(1, limit)
-    val boi = List.range(1,3,10).toSet
-    val Set[Int] multiples_1 = numbers.filter(_ % 3 == 0)
-    val multiples_2 = numbers.filter(_ % 5 == 0)
-    val multiples = List.concat(multiples_1, multiples_2)
-
-    def sum(list: List[Int]): Int = list match {
-        case Nil => 0
-        case x :: xs => x + sum(xs)
+    var removed = List.range(1, limit)
+    for (factor <- factors){
+      removed = removed.filterNot(_ % factor == 0)
     }
-    sum(multiples_1)
+    List.range(1, limit).sum - removed.sum
   }
 }
