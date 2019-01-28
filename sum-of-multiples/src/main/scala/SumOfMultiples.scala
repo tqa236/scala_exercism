55ad1f7c016b20c196e279c5dcb28afe44c4bca6
TQA <trinhquocanh94@gmail.com>
2019-01-28 12:44:54 +0100

    Improve sum of multiples solution.

--- sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,12 +1,5 @@
 object SumOfMultiples {
   def sum(factors: Set[Int], limit: Int): Int = {
-    val factor_list = List.empty[Int] ++ factors
-    def remove(factor_list: List[Int], removed: List[Int]) : List[Int] = {
-      factor_list match {
-        case Nil => removed
-        case x :: xs => remove(xs, removed.filterNot(_ % x == 0))
-      }
-    }
-    List.range(1, limit).sum - remove(factor_list, List.range(1, limit)).sum
+    List.range(1, limit).filter(x => factors.exists(x % _ == 0)).sum
   }
 }
