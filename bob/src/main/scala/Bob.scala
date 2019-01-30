72c7ec7e4fc1344119213a6e1b12a746a5cdad5f
TQA <trinhquocanh94@gmail.com>
2019-01-30 11:42:42 +0100

    Solve bob exercise.

--- bob/src/main/scala/Bob.scala
+++ bob/src/main/scala/Bob.scala
@@ -1,3 +1,13 @@
 object Bob {
-  def response(statement: String): String = "Whatever."
+  def response(statement: String): String = {
+    ( statement.exists(_.isLower), statement.replaceAll("\\s", "").takeRight(1) == "?", statement.exists(_.isUpper), statement.replaceAll("\\s", "") == "") match {
+      case (other, other2, other3, true) => "Fine. Be that way!"
+      case (false, false, false, other) => "Whatever."
+      case (false, true, false, other) => "Sure."
+      case (false, true, other, other2) => "Calm down, I know what I'm doing!"
+      case (false, false, other, other2) => "Whoa, chill out!"
+      case (other, true, other2, other3) => "Sure."
+      case other => "Whatever."
+    }
+  }
 }
--- bob/src/test/scala/BobTest.scala
+++ bob/src/test/scala/BobTest.scala
@@ -8,130 +8,130 @@ class BobTest extends FunSuite with Matchers {
   }
 
   test("shouting") {
-    pending
+    // pending
     Bob.response("WATCH OUT!") should be("Whoa, chill out!")
   }
 
   test("shouting gibberish") {
-    pending
+    // pending
     Bob.response("FCECDFCAAB") should be("Whoa, chill out!")
   }
 
   test("asking a question") {
-    pending
+    // pending
     Bob.response("Does this cryogenic chamber make me look fat?") should be(
       "Sure.")
   }
 
   test("asking a numeric question") {
-    pending
+    // pending
     Bob.response("You are, what, like 15?") should be("Sure.")
   }
 
   test("asking gibberish") {
-    pending
+    // pending
     Bob.response("fffbbcbeab?") should be("Sure.")
   }
 
   test("talking forcefully") {
-    pending
+    // pending
     Bob.response("Let's go make out behind the gym!") should be("Whatever.")
   }
 
   test("using acronyms in regular speech") {
-    pending
+    // pending
     Bob.response("It's OK if you don't want to go to the DMV.") should be(
       "Whatever.")
   }
 
   test("forceful question") {
-    pending
+    // pending
     Bob.response("WHAT THE HELL WERE YOU THINKING?") should be(
       "Calm down, I know what I'm doing!")
   }
 
   test("shouting numbers") {
-    pending
+    // pending
     Bob.response("1, 2, 3 GO!") should be("Whoa, chill out!")
   }
 
   test("no letters") {
-    pending
+    // pending
     Bob.response("1, 2, 3") should be("Whatever.")
   }
 
   test("question with no letters") {
-    pending
+    // pending
     Bob.response("4?") should be("Sure.")
   }
 
   test("shouting with special characters") {
-    pending
+    // pending
     Bob.response("ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!") should be(
       "Whoa, chill out!")
   }
 
   test("shouting with no exclamation mark") {
-    pending
+    // pending
     Bob.response("I HATE THE DMV") should be("Whoa, chill out!")
   }
 
   test("statement containing question mark") {
-    pending
+    // pending
     Bob.response("Ending with ? means a question.") should be("Whatever.")
   }
 
   test("non-letters with question") {
-    pending
+    // pending
     Bob.response(":) ?") should be("Sure.")
   }
 
   test("prattling on") {
-    pending
+    // pending
     Bob.response("Wait! Hang on. Are you going to be OK?") should be("Sure.")
   }
 
   test("silence") {
-    pending
+    // pending
     Bob.response("") should be("Fine. Be that way!")
   }
 
   test("prolonged silence") {
-    pending
+    // pending
     Bob.response("          ") should be("Fine. Be that way!")
   }
 
   test("alternate silence") {
-    pending
+    // pending
     Bob.response("										") should be("Fine. Be that way!")
   }
 
   test("multiple line question") {
-    pending
+    // pending
     Bob.response("""
 Does this cryogenic chamber make me look fat?
 No.""") should be("Whatever.")
   }
 
   test("starting with whitespace") {
-    pending
+    // pending
     Bob.response("         hmmmmmmm...") should be("Whatever.")
   }
 
   test("ending with whitespace") {
-    pending
+    // pending
     Bob.response("Okay if like my  spacebar  quite a bit?   ") should be(
       "Sure.")
   }
 
   test("other whitespace") {
-    pending
+    // pending
     Bob.response("""
                  	""") should be("Fine. Be that way!")
   }
 
   test("non-question ending with whitespace") {
-    pending
+    // pending
     Bob.response("This is a statement ending with whitespace      ") should be(
       "Whatever.")
   }
