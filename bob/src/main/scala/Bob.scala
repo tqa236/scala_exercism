628cca65c5f64dbe82da9d4dd6e1fb9506aec886
TQA <trinhquocanh94@gmail.com>
2019-02-07 08:27:12 +0100

    Improve bob solution

--- bob/src/main/scala/Bob.scala
+++ bob/src/main/scala/Bob.scala
@@ -1,13 +1,15 @@
 object Bob {
   def response(statement: String): String = {
-    ( statement.exists(_.isLower), statement.replaceAll("\\s", "").takeRight(1) == "?", statement.exists(_.isUpper), statement.replaceAll("\\s", "") == "") match {
-      case (other, other2, other3, true) => "Fine. Be that way!"
-      case (false, false, false, other) => "Whatever."
-      case (false, true, false, other) => "Sure."
-      case (false, true, other, other2) => "Calm down, I know what I'm doing!"
-      case (false, false, other, other2) => "Whoa, chill out!"
-      case (other, true, other2, other3) => "Sure."
-      case other => "Whatever."
-    }
+      val isEmpty = statement.replaceAll("\\s", "") == ""
+      val isQuestion = statement.replaceAll("\\s", "").takeRight(1) == "?"
+      val isYelling = statement.exists(_.isUpper) && !statement.exists(_.isLower)
+
+      if (isEmpty) "Fine. Be that way!"
+      else if (isQuestion){
+          if (isYelling) "Calm down, I know what I'm doing!"
+          else "Sure."
+      }
+      else if (isYelling) "Whoa, chill out!"
+      else "Whatever."
   }
 }
