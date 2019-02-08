f1300e678b0965b397e1f19bf362453050d7b4d2
TQA <trinhquocanh94@gmail.com>
2019-02-08 09:40:16 +0100

    Improve space age solution

--- space-age/src/main/scala/SpaceAge.scala
+++ space-age/src/main/scala/SpaceAge.scala
@@ -1,26 +1,13 @@
 object SpaceAge{
-    def onEarth(seconds: Double): Double = {
-        seconds/31557600.0
+    private def onPlanet(seconds: Double, earthYear: Double): Double = {
+        seconds/(31557600.0*earthYear)
     }
-    def onMercury(seconds: Double): Double = {
-        onEarth(seconds)/ 0.2408467
-    }
-    def onVenus(seconds: Double): Double = {
-        onEarth(seconds)/ 0.61519726
-    }
-    def onMars(seconds: Double): Double = {
-        onEarth(seconds)/1.8808158
-    }
-    def onJupiter(seconds: Double): Double = {
-        onEarth(seconds)/ 11.862615
-    }
-    def onSaturn(seconds: Double): Double = {
-        onEarth(seconds)/ 29.447498
-    }
-    def onUranus(seconds: Double): Double = {
-        onEarth(seconds)/ 84.016846
-    }
-    def onNeptune(seconds: Double): Double = {
-        onEarth(seconds)/ 164.79132
-    }
+    def onEarth(seconds: Double): Double = onPlanet(seconds, 1.0)
+    def onMercury(seconds: Double): Double = onPlanet(seconds, 0.2408467)
+    def onVenus(seconds: Double): Double = onPlanet(seconds, 0.61519726)
+    def onMars(seconds: Double): Double = onPlanet(seconds,1.8808158)
+    def onJupiter(seconds: Double): Double = onPlanet(seconds, 11.862615)
+    def onSaturn(seconds: Double): Double = onPlanet(seconds, 29.447498)
+    def onUranus(seconds: Double): Double = onPlanet(seconds, 84.016846)
+    def onNeptune(seconds: Double): Double = onPlanet(seconds, 164.79132)
 }
