695d4c68acd7c2426281ff522e806ca58a7cdf52
TQA <trinhquocanh94@gmail.com>
2019-02-20 11:04:57 +0100

    Refactor

--- darts/src/main/scala/Darts.scala
+++ darts/src/main/scala/Darts.scala
@@ -3,9 +3,11 @@ import scala.math.sqrt
 object Darts{
     def score(x: Double, y: Double): Int = {
         val distance = sqrt(x*x + y*y)
-        if (distance > 10) 0
-        else if (distance > 5) 1
-        else if (distance > 1) 5
-        else 10
+        distance match {
+            case i if i > 10 => 0
+            case i if i > 5 => 1
+            case i if i > 1 => 5
+            case _ => 10
+        }
     }
 }
