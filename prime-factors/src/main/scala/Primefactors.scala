66cf1efde72adc700ac073d457f902ef0722c073
TQA <trinhquocanh94@gmail.com>
2019-03-12 22:14:34 +0100

    Solve prime factors

--- prime-factors/src/main/scala/Primefactors.scala
+++ prime-factors/src/main/scala/Primefactors.scala
@@ -1,6 +1,14 @@
 object PrimeFactors{
     def factors(number: Long): List[Long] = number match {
-        case 1 => List()
-        case number if number % 2 == 0 => List(2)
+        case 1 => List.empty[Long]
+        case number if number % 2 == 0 => 2 :: factors(number/2)
+        case number => factorsWithOddFactor(number, 3)
     }
+
+    def factorsWithOddFactor(number: Long, factor: Long): List[Long] = number match {
+        case 1 => List.empty[Long]
+        case number if number % factor == 0 => factor :: factorsWithOddFactor(number/factor, factor)
+        case number => factorsWithOddFactor(number, factor + 2)
+    }
+
 }
