fe9edab90f1ad2ca1b946390e5b832b426ed5c7e
TQA <trinhquocanh94@gmail.com>
2019-03-17 15:39:33 +0100

    Refactor

--- all-your-base/src/main/scala/AllYourBase.scala
+++ all-your-base/src/main/scala/AllYourBase.scala
@@ -1,10 +1,10 @@
 object AllYourBase{
-    def rebase(inBase: Int, inDigits: List[Int], outBase: Int): Option[List[Int]] = {
-        if (inBase < 2 || outBase < 2) None
-        else if (inDigits == List()) Some(List(0))
-        else if (inDigits.min < 0 || inDigits.max >= inBase) None
-        else if (inDigits.sum == 0) Some(List(0))
-        else Some(decToBase(baseToDec(inBase, inDigits), outBase).reverse)
+    def rebase(inBase: Int, inDigits: List[Int], outBase: Int): Option[List[Int]] = inDigits match {
+        case inDigits if (inBase < 2 || outBase < 2) => None
+        case inDigits if (inDigits == List()) => Some(List(0))
+        case inDigits if (inDigits.min < 0 || inDigits.max >= inBase) => None
+        case inDigits if (inDigits.sum == 0) => Some(List(0))
+        case _ => Some(decToBase(baseToDec(inBase, inDigits), outBase).reverse)
     }
 
     def baseToDec(inBase: Int, inDigits: List[Int]): Int =
--- largest-series-product/src/main/scala/Series.scala
+++ largest-series-product/src/main/scala/Series.scala
@@ -1,8 +1,8 @@
 object Series{
-    def largestProduct(size: Int, number: String): Option[Int] = {
-        if (size == 0) Some(1)
-        else if (size < 0 || size > number.length) None
-        else if (!(number forall Character.isDigit)) None
-        else Some(number.map(_.asDigit).sliding(size).map(_.product).max)
+    def largestProduct(size: Int, number: String): Option[Int] = size match {
+        case 0 => Some(1)
+        case size if (size < 0 || size > number.length) => None
+        case size if (!(number forall Character.isDigit)) => None
+        case _ => Some(number.map(_.asDigit).sliding(size).map(_.product).max)
     }
 }
