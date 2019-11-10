8eeab3c2cfad556c97826fada870adf1b0b8d5b6
TQA <trinhquocanh94@gmail.com>
2019-11-10 23:16:45 +0100

    Add coverage

---
+++ coverage.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Automatically test all exercises
+for path in */; do
+    [ -d "${path}" ] || continue # if not a directory, skip
+    # [[ $path = ^a\(.*?\) ]] || echo "$path"  continue
+    dirname="$(basename "${path}")"
+    cp "plugins.sbt" "$dirname/project/plugins.sbt"
+    cd "$dirname" || exit
+    sbt clean coverage test coverageReport
+    cd .. || exit
+done
