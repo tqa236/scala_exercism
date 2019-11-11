ebb9f12d09370bd1bdb3e607bf615f655bd31efc
TQA <trinhquocanh94@gmail.com>
2019-11-11 11:44:07 +0100

    Fix submitting to coveralls

---
+++ coveralls.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Automatically test all exercises
+for path in */; do
+    [ -d "${path}" ] || continue # if not a directory, skip
+    dirname="$(basename "${path}")"
+    cd "$dirname" || exit
+    sbt coveralls
+    cd .. || exit
+done
