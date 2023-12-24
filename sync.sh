8a3b8e2ebc9562546b974f1ece7de1e704d91085
Anh Trinh <trinhquocanh94@gmail.com>
2023-12-24 23:02:03 +0100

    add sync script

---
+++ sync.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+for path in */; do
+    [ -d "${path}" ] || continue # if not a directory, skip
+    dirname="$(basename "${path}")"
+    cd "$dirname" || exit
+    exercise_name=${path::-1}
+    file_name=${file_name//-/_}
+    echo $file_name
+    exercism download --track=python --exercise="${exercise_name}" --force
+    sleep 1
+    cd .. || exit
+done
